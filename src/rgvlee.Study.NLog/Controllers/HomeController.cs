9d78902def0bf46b87dfec1352d001a0be6843d5
rgvlee <lee@rgvlee.com>
2020-08-29 21:54:28 +0800

    Added cases to observe default behaviour

--- src/rgvlee.Study.NLog/Controllers/HomeController.cs
+++ src/rgvlee.Study.NLog/Controllers/HomeController.cs
@@ -22,6 +22,52 @@ namespace rgvlee.Study.NLog.Controllers
         public IActionResult Index()
         {
             _logger.LogInformation("Hello, this is the index!");
+            
+            try
+            {
+                _logger.LogInformation("Placeholder with no value {key1}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Placeholder with no value failed", ex);
+            }
+
+            try
+            {
+                _logger.LogInformation("More placeholders than values {key1}, {key2}", "value1");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("More placeholders than values failed", ex);
+            }
+
+            try
+            {
+                _logger.LogInformation("Null params {key1}", null);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Null params failed", ex);
+            }
+
+            try
+            {
+                _logger.LogInformation("One null value {key1}, {key2}", null, "value2");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("One null value failed", ex);
+            }
+
+            try
+            {
+                _logger.LogInformation("Two null values {key1}, {key2}", null, null);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Two null values failed", ex);
+            }
+
             return View();
         }
 
